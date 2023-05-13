c069a4b58db5727b77af4bf4d6e23fb3187c8a99
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-14 00:59:50 +0800

    feat: search function

--- client/admin/account-registration.php
+++ client/admin/account-registration.php
@@ -95,7 +95,7 @@ include "../../server/db/conDB.php";
                 </button>
             </a>
             <!--search bar-->
-            <form>
+            <form autocomplete="off">
                 <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                 <div class="relative w-96">
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -103,8 +103,7 @@ include "../../server/db/conDB.php";
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                         </svg>
                     </div>
-                    <input type="search" id="default-search" class="border-0 block w-full p-4 pl-10 text-sm text-black rounded-lg bg-white focus:border-gray-500" placeholder="Search" required>
-                    <button type="submit" class="text-white absolute right-2.5 bottom-2 bg-blue-600 hover:bg-blue-500 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:focus:ring-blue-800">Search</button>
+                    <input onkeyup="mySearch()" type="search" id="default-search" class="border-0 block w-full p-4 pl-10 text-sm text-black rounded-lg bg-white focus:border-gray-500" placeholder="Search">
                 </div>
             </form>
         </div>
@@ -112,7 +111,7 @@ include "../../server/db/conDB.php";
         <!--table-->
         <div class="p-4 h-screen overflow-y-auto ">
             <div class="relative overflow-y-auto h-full bg-white shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <table id="table" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="sticky top-0 text-xs text-white uppercase bg-black">
                         <tr>
                             <th scope="col" class="px-6 py-3">
@@ -273,6 +272,32 @@ include "../../server/db/conDB.php";
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+    <script>
+        function mySearch() {
+            // Declare variables
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("default-search");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("table");
+            tr = table.getElementsByTagName("tr");
+            // Loop through all table rows, and hide those who don't match the search query
+            for (i = 1; i < tr.length; i++) {
+                column1 = tr[i].getElementsByTagName("td")[0];
+                column2 = tr[i].getElementsByTagName("td")[1];
+                column3 = tr[i].getElementsByTagName("td")[2];
+                if (column1 && column2 && column3) {
+                    column1 = column1.textContent || column1.innerText;
+                    column2 = column2.textContent || column2.innerText;
+                    column3 = column3.textContent || column3.innerText;
+                    if (column1.toUpperCase().indexOf(filter) > -1 || column2.toUpperCase().indexOf(filter) > -1 || column3.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
+    </script>
 </body>
 
 </html>
