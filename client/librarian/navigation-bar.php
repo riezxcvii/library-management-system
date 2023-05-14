96eaadfefc11cbdabbaa0b69eb3d99f046f50274
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-14 23:51:25 +0800

    :truck: fix: move file and fix paths

--- client/librarian/navigation-bar.php
+++ client/librarian/navigation-bar.php
@@ -81,7 +81,7 @@
                         </svg>
                         <h3 class="mb-5 text-lg font-normal text-gray-900">Are you sure you want to
                             sign out?</h3>
-                        <a href="../admin/logout.php?id=<?php echo $admin_ID; ?>">
+                        <a href="../../server/admin/logout.php?id=<?php echo $admin_ID; ?>">
                             <button type="button" class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                                 Yes, I'm sure
                             </button>
--- server/admin/logout.php
+++ server/admin/logout.php
@@ -1,6 +1,6 @@
 <?php
-include "../../server/db/conDB.php";
+include "../db/conDB.php";
 $sql = "UPDATE `log_history` SET time_out = NOW() WHERE date = CURDATE() ORDER BY time_in DESC LIMIT 1 ";
 mysqli_query($conn, $sql);
 session_destroy();
-header("Location: ../index.php");
+header("Location: ../../client/index.php");
