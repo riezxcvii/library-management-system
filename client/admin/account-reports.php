4e13f06b1a4b279ce90d93aa86a3dfae23bf1c12
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-14 22:57:14 +0800

    :card_file_box: fix: connected badge in db

--- client/admin/account-reports.php
+++ client/admin/account-reports.php
@@ -8,12 +8,16 @@ include('navigation-bar.php');
     <button type="button" class="mr-8 inline-flex items-center px-5 py-2.5 text-m font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300" data-modal-target="register-modal" data-modal-toggle="register-modal">
         Register a borrower
     </button>
+
+    <?php
+    $pending = mysqli_query($conn, "SELECT COUNT(status) as total FROM borrowers where status='0' ;");
+    $p = mysqli_fetch_assoc($pending)
+    ?>
+
     <a href="account-registration.php" class="flex mr-8">
         <button type="button" class="inline-flex items-center px-5 py-2.5 text-m font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300">
             Pending Registration
-            <span class="inline-flex items-center justify-center w-8 h-8 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
-                99
-            </span>
+            <span class="inline-flex items-center justify-center w-8 h-8 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full"><?php echo $p['total']; ?></span>
         </button>
     </a>
     <!--search bar-->
