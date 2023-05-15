af65244bb7a9c30062cfb83ef74456d9c661a1a0
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-15 11:18:31 +0800

    :sparkles: feat: added function for search bar

--- client/librarian/all-books.php
+++ client/librarian/all-books.php
@@ -16,8 +16,7 @@ include('navigation-bar.php');
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                 </svg>
             </div>
-            <input type="search" id="default-search" class="border-0 block w-full p-4 pl-10 text-sm text-black rounded-lg bg-white focus:border-gray-500" placeholder="Search" required>
-            <button type="submit" class="text-white absolute right-2.5 bottom-2 bg-blue-600 hover:bg-blue-500 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:focus:ring-blue-800">Search</button>
+            <input type="search" id="default-search" class="border-0 block w-full p-4 pl-10 text-sm text-black rounded-lg bg-white focus:border-gray-500" placeholder="Search" onkeyup="mySearch()">
         </div>
     </form>
 </div>
@@ -25,7 +24,7 @@ include('navigation-bar.php');
 <!--table-->
 <div class="p-4 h-screen overflow-y-auto ">
     <div class="relative overflow-y-auto h-full bg-white shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <table id="table" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="sticky top-0 text-xs text-white uppercase bg-black">
                 <tr>
                     <th scope="col" class="px-6 py-3">
@@ -419,6 +418,36 @@ include('navigation-bar.php');
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+<script>
+    function mySearch() {
+        // Declare variables
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("default-search");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("table");
+        tr = table.getElementsByTagName("tr");
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 1; i < tr.length; i++) {
+            column1 = tr[i].getElementsByTagName("td")[0];
+            column2 = tr[i].getElementsByTagName("td")[1];
+            column3 = tr[i].getElementsByTagName("td")[2];
+            column4 = tr[i].getElementsByTagName("td")[3];
+            column5 = tr[i].getElementsByTagName("td")[4];
+            if (column1 && column2 && column3 && column4 && column5) {
+                column1 = column1.textContent || column1.innerText;
+                column2 = column2.textContent || column2.innerText;
+                column3 = column3.textContent || column3.innerText;
+                column4 = column4.textContent || column4.innerText;
+                column5 = column5.textContent || column5.innerText;
+                if (column1.toUpperCase().indexOf(filter) > -1 || column2.toUpperCase().indexOf(filter) > -1 || column3.toUpperCase().indexOf(filter) > -1 || column4.toUpperCase().indexOf(filter) > -1 || column5.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+</script>
 </body>
 
 </html>
