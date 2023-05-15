f3a388f298c6a1324b7d86b338c07e6bdddc7bcd
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-15 12:01:48 +0800

    :sparkles: feat: added function for print button

---
+++ server/print/all-books.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<?php
+require '../db/conDB.php';
+?>
+<html lang="en">
+
+<head>
+	<title>ALMS: Print Report</title>
+	<link href="../../client/assets/logo.png" type="image/x-icon" rel="shortcut icon">
+</head>
+
+<style>
+	body {
+		font-family: Arial, sans-serif;
+		margin: 0.5in;
+	}
+
+	.table {
+		width: 100%;
+	}
+
+	.table-striped tbody>tr:nth-child(odd)>td,
+	.table-striped tbody>tr:nth-child(odd)>th {
+		background-color: #f9f9f9;
+	}
+
+	@media print {
+		#print {
+			display: none;
+		}
+	}
+
+	@media print {
+		#PrintButton {
+			display: none;
+		}
+	}
+
+	@page {
+		size: auto;
+		/* auto is the initial value */
+		margin: 0;
+		/* this affects the margin in the printer settings */
+	}
+</style>
+
+<body>
+	<div>
+		<img src="../../client/assets/logo.png" style="display:block; margin:0 auto 0 auto; width:6%;">
+		<h4 style="text-align:center; font-weight:500; margin-top:1%">REPUBLIC OF THE PHILPPINES</h4>
+		<h4 style="text-align:center; font-weight:500; margin-top:-1.5%">ANTIQUE NATIONAL SCHOOL</h4>
+		<h4 style="text-align:center; font-weight:500; margin-top:-1.5%">T.A. FORNIER ST., SAN JOSE, ANTIQUE</h4>
+		<br>
+		<h4 style="text-align:center; margin-top:-1.5%">ANS LIBRARY REPORT</h4>
+		<h4 style="text-align:center; font-weight:500; margin-top:-1.5%">LIST OF BOOKS</h4>
+	</div>
+
+	<br>
+	<b style="text-align:right">DATE PREPARED:</b>
+	<?php
+	$date = date("m.d.Y", strtotime("+6 HOURS"));
+	echo $date;
+	?>
+	<br><br>
+
+	<table id="table" class="result10" style="border:1px solid black; border-collapse:collapse; width:100%">
+		<tr>
+			<td id="ISBN10" id="colNo12" style="font-weight:bold; text-align:center; width:8%; border:1px solid black">ISBN</td>
+			<td id="title10" id="colNo12" style="font-weight:bold; text-align:center; width:15%; border:1px solid black">TITLE</td>
+			<td id="author10" id="colNo12" style="font-weight:bold; text-align:center; width:12%; border:1px solid black">AUTHOR</td>
+			<td id="stat10" id="colNo12" style="font-weight:bold; text-align:center; width:3%; border:1px solid black">CATEGORY</td>
+			<td id="stat10" id="colNo12" style="font-weight:bold; text-align:center; width:3%; border:1px solid black">AVAILABLE COPIES</td>
+		</tr>
+
+		<?php
+		require '../db/conDB.php';
+		$query = $conn->query("SELECT * FROM `books`");
+		while ($fetch = $query->fetch_array()) {
+		?>
+
+			<tr>
+				<td style="border:1px solid black; padding:1.5px; padding-top:3px; text-align:center"><?php echo $fetch['isbn'] ?></td>
+				<td style="border:1px solid black; padding:1.5px; padding-left:10px; padding-top:3px"><?php echo $fetch['title'] ?></td>
+				<td style="border:1px solid black; padding:1.5px; padding-left:10px; padding-top:3px"><?php echo $fetch['author_firstname'] ?> <?php echo $fetch['author_lastname'] ?></td>
+				<td style="border:1px solid black; padding:1.5px; padding-top:3px; text-align:center"><?php echo $fetch['category'] ?></td>
+				<td style="border:1px solid black; padding:1.5px; padding-top:3px; text-align:center"><?php echo $fetch['copies'] ?></td>
+			</tr>
+
+		<?php
+		}
+		?>
+	</table>
+
+</body>
+
+<script type="text/javascript">
+	function PrintPage() {
+		window.print();
+	}
+	document.loaded = function() {}
+	window.addEventListener('DOMContentLoaded', (event) => {
+		PrintPage()
+		setTimeout(function() {
+			window.close()
+		}, 750)
+	});
+</script>
+
+</html>
