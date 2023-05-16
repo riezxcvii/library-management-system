a51b7f7412a3f04f9731a4bf5a1816b5a251ca8a
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-16 18:44:46 +0800

    :truck: fix: display data in details popup form

---
+++ client/admin/update_borrower_status.php
@@ -0,0 +1,10 @@
+<?php
+
+include "../../server/db/conDB.php";
+// Get the borrower ID and status from the AJAX request
+$borrowerId = $_POST['borrowerId'];
+$status = $_POST['status'];
+
+
+$sql=" UPDATE borrowers SET status = :status WHERE borrower_ID = :borrowerId";
+$res=mysqli_query($conn,$sql);
