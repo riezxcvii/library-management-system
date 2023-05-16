92e5f0a5108f635ba8c12ce4b409d19d6cb9b926
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-16 19:26:01 +0800

    :sparkle: fix: display data in the table

--- client/admin/account-management.php
+++ client/admin/account-management.php
@@ -57,6 +57,14 @@ include('navigation-bar.php');
                 $borrower = $conn->query("SELECT * FROM `borrowers` WHERE status=1 and deactivate = 0") or die(mysqli_error($conn));
                 while ($name = $borrower->fetch_array()) {
                     $id = $name['borrower_ID'];
+                    $idNumber = $name['id_number'];
+                    $last = $name['last_name'];
+                    $first = $name['first_name'];
+                    $middle = $name['middle_initial'];
+                    $extension = $name['name_extension'];
+                    $idNumber = $name['id_number'];
+                    $sex = $name['sex'];
+                    $role = $name['role'];
                 ?>
 
                     <tr class="bg-white border-b text-black font-semibold">
@@ -102,31 +110,45 @@ include('navigation-bar.php');
             <div class="px-6 py-6 lg:px-8">
                 <h3 class="mb-4 text-xl font-medium text-gray-900 text-center">Registration Details</h3>
                 <form class="space-y-6" action="#" autocomplete="off">
-                    <div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900">ID Number</label>
-                        <input type="number" name="employeeID" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" onKeyPress="if(this.value.length==15) return false;" disabled>
+
+                    <div class="grid grid-cols-2 gap-4">
+                        <div>
+                            <label class="block mb-2 text-sm font-medium text-gray-900">ID Number</label>
+                            <input type="number" name="employeeID" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" onKeyPress="if(this.value.length==15) return false;" disabled value="<?php echo $idNumber ?>">
+                        </div>
+
+                        <div>
+                            <label class="block mb-2 text-sm font-medium text-gray-900">Role</label>
+                            <select id="nameExtension" class="bg-gray-50 border border-gray-400 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" disabled>
+                                <option value="" selected><?php echo $role ?></option>
+                                <option value="Admin">Admin</option>
+                                <option value="Librarian">Librarian</option>
+                                <option value="Student">Student</option>
+                                <option value="Teacher">Teacher</option>
+                            </select>
+                        </div>
                     </div>
 
                     <div class="grid grid-cols-3 gap-4">
                         <div>
                             <label class="block mb-2 text-sm font-medium text-gray-900">Last Name</label>
-                            <input type="text" name="lastName" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="20" disabled>
+                            <input type="text" name="lastName" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="20" disabled value="<?php echo $last ?>">
                         </div>
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900">First
                                 Name</label>
-                            <input type="text" name="firstName" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="25" disabled>
+                            <input type="text" name="firstName" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="25" disabled value="<?php echo $first ?>">
                         </div>
                         <div>
                             <label class="block mb-2 text-sm font-medium text-gray-900">Middle
                                 Initial</label>
-                            <input type="text" name="middleInitial" class="text-center bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="1" disabled>
+                            <input type="text" name="middleInitial" class="text-center bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="1" disabled value="<?php echo $middle ?>">
                         </div>
                     </div>
                     <div class="grid grid-cols-2 gap-4">
                         <div>
                             <label class="block mb-2 text-sm font-medium text-gray-900">Name Extension</label>
-                            <select id="nameExtension" class="bg-gray-50 border border-gray-400 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-center" disabled>
+                            <select id="nameExtension" class="bg-gray-50 border border-gray-400 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-center" disabled value="<?php echo $extension ?>">
                                 <option value="" selected>Select Extension</option>
                                 <option value="Jr">Jr</option>
                                 <option value="Sr">Sr</option>
@@ -145,11 +167,15 @@ include('navigation-bar.php');
                         <div class="flex">
                             <label class="block text-sm font-medium text-gray-900">Sex</label>
                             <div class="flex items-center mb-4 mr-4">
-                                <input id="female" type="radio" value="Female" name="female" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 mt-5 ml-[-1rem]" disabled>
+                                <input id="female" <?php if ($sex == 'Female') {
+                                                        echo 'checked';
+                                                    } ?> type="radio" value="Female" name="sex" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 mt-5 ml-[-1rem]" disabled>
                                 <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 mt-5">Female</label>
                             </div>
                             <div class="flex items-center mb-4">
-                                <input id="male" type="radio" value="Male" name="male" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 mt-5" disabled>
+                                <input id="male" <?php if ($sex == 'Male') {
+                                                        echo 'checked';
+                                                    } ?> type="radio" value="Male" name="sex" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 mt-5" disabled>
                                 <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 mt-5">Male</label>
                             </div>
                         </div>
