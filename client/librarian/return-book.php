4bc25c4c8db5915f7384b2e3e6f755501db486d4
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-20 14:40:12 +0800

    :construction: feat: connected table in the db, added card catalog

--- client/librarian/return-book.php
+++ client/librarian/return-book.php
@@ -19,13 +19,13 @@ include('navigation-bar.php');
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                 </svg>
             </div>
-            <input type="search" id="default-search" class="border-0 block w-full p-4 pl-10 text-sm text-black rounded-lg bg-white focus:border-gray-500" placeholder="Search" required  onkeyup="mySearch()">
+            <input type="search" id="default-search" class="border-0 block w-full p-4 pl-10 text-sm text-black rounded-lg bg-white focus:border-gray-500" placeholder="Search" required onkeyup="mySearch()">
         </div>
     </form>
 </div>
 
 <!--table-->
-<div class="p-4 h-screen overflow-y-auto ">
+<div class="p-4 h-screen overflow-y-auto">
     <div class="relative overflow-y-auto h-full bg-white shadow-md sm:rounded-lg">
         <table id="table" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="sticky top-0 text-xs text-white uppercase bg-black">
@@ -78,38 +78,166 @@ include('navigation-bar.php');
                                 </svg></a>
                         </div>
                     </th>
+                    <th scope="col" class="px-6 py-3">
+                        <div class="flex items-center">
+                            Due Date
+                            <a href="#" onclick="sortTable(6)"><svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512">
+                                    <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                                </svg></a>
+                        </div>
+                    </th>
                     <th scope="col" class="px-6 py-3 flex justify-center">
                         Action
                     </th>
             </thead>
             <tbody>
-                <tr class="bg-white border-b text-black font-semibold">
-                    <th scope="row" class="px-6 py-2 font-semibold text-black whitespace-nowrap">
-                        Rieza Marie Banquillo
-                    </th>
-                    <td class="px-6 py-2">
-                        Noli Me Tangere
-                    </td>
-                    <td class="px-6 py-2">
-                        Jose Rizal
-                    </td>
-                    <td class="px-6 py-2">
-                        Borrowed
-                    </td>
-                    <td class="px-6 py-2">
-                        03-20-2023
-                    </td>
-                    <td class="px-6 py-2">
 
-                    </td>
-                    <td class="px-6 py-2 justify-center flex">
-                        <button type="button" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-2 focus:outline-none focus:ring-blue-300">
-                            Return
-                        </button>
-                    </td>
-                </tr>
+                <?php
+                $qreturn = $conn->query("SELECT b.*, u.*, br.* FROM borrowed_books AS br
+        INNER JOIN books AS b ON br.book_ID = b.book_ID
+        INNER JOIN borrowers AS u ON br.borrower_ID = u.borrower_ID") or die(mysqli_error($con));
+                while ($freturn = $qreturn->fetch_array()) {
+                    $id = $freturn['borrow_ID'];
+                    $dueDate = $freturn['due_date'];
+
+                ?>
+
+                    <tr class="bg-white border-b text-black font-semibold">
+                        <th scope="row" class="px-6 py-2 font-semibold text-black whitespace-nowrap">
+                            <?php echo $freturn['first_name'] . ' ' . $freturn['last_name']  ?>
+                        </th>
+                        <td onclick="openModal(<?php echo $ID; ?>)" class="px-6 py-2 select-none hover:bg-blue-200" data-modal-target="card-modal" data-modal-toggle="card-modal">
+                            <?php
+                            $qbook = $conn->query("SELECT * FROM `books` WHERE `book_ID` = '$freturn[book_ID]'") or die(mysqli_error($conn));
+                            $fbook = $qbook->fetch_array();
+                            echo $fbook['title'];
+                            ?>
+                        </td>
+                        <td class="px-6 py-2">
+                            <?php
+                            $qbook = $conn->query("SELECT * FROM `books` WHERE `book_ID` = '$freturn[book_ID]'") or die(mysqli_error($conn));
+                            $fbook = $qbook->fetch_array();
+                            echo $fbook['author_firstname'] . " " . $fbook['author_lastname'];
+                            ?>
+                        </td>
+                        <td class="px-6 py-2">
+                            <?php echo $freturn['status'] ?>
+                        </td>
+                        <td class="px-6 py-2">
+                            <?php echo $freturn['date_issued'] ?>
+                        </td>
+                        <td class="px-6 py-2">
+                            <?php echo $freturn['returned_date'] ?>
+                        </td>
+                        <?php
+                        if ($dueDate <= $date) {
+                        ?>
+                            <td onselectstart="return false" style="color:red;"><?php echo $freturn['due_date']; ?></td>
+
+                            <td>
+                            <?php
+
+                        } else {
+                            ?>
+                            <td class="px-6 py-2">
+                                <?php echo $freturn['due_date'] ?>
+                            </td>
+
+                            <td class="px-6 py-2 justify-center flex">
+                            <?php
+                        }
+                            ?>
+
+                            <?php
+                            if ($freturn['status'] == 'Returned') {
+                                echo '<center><button disabled = "disabled" class = "btn btn-primary" type = "button" href = "#" id="returned6" data-toggle = "modal" data-target = "#return"><span class = "glyphicon glyphicon-check"></span>RETURNED</button></center>';
+                            } else {
+                            ?>
+                                <a href="return.php?id=<?php echo $id; ?>">
+                                    <button class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-2 focus:outline-none focus:ring-blue-300">RETURN</button>
+                                </a>
+                            <?php
+                            }
+                            ?>
+                            </td>
+                    </tr>
+                <?php
+                }
+                ?>
             </tbody>
         </table>
+    </div>
+</div>
+
+<!--card modal-->
+<div id="card-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative max-h-full mx-auto flex items-center justify-center w-[55rem]">
+        <!--card modal content-->
+        <div class="relative bg-gray-200 rounded-lg shadow">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="card-modal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="px-6 py-6 lg:px-8">
+                <h3 class="mb-4 text-xl font-medium text-gray-900 text-center">Card Catalog</h3>
+                <form class="space-y-6" action="#" autocomplete="off">
+                    <table>
+                        <tr>
+                            <td id="category" class="text-center px-8 py-1"></td>
+                            <td></td>
+                        </tr>
+
+                        <tr>
+                            <td id="authorNumber" class="text-center px-8 py-1"></td>
+                            <td></td>
+                        </tr>
+
+
+                        <tr>
+                            <td></td>
+                            <td id="author" class="px-8 py-1"></td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td id="title" class="pl-16 px-8 py-1"></td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td id="publication" class="px-8 py-1"></td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td id="physical" class="px-8 py-1">20 p. : col. ill.</td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td id="isbn" class="px-8 py-1"></td>
+                        </tr>
+
+                        <tr>
+                            <td id="accessionNumber" class="px-8 py-1"></td>
+                            <td></td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td id="subject" class="px-8 py-1"></td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td id="tracing" class="px-8 py-1"></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 
---
+++ client/librarian/return.php
@@ -0,0 +1,14 @@
+<?php
+include("../../server/db/conDB.php");
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $return = date("Y-m-d", strtotime("+8 HOURS"));
+    $sql = "UPDATE borrowed_books SET status = 'Returned', returned_date = '$return' WHERE borrow_ID = '$id'";
+    $res = mysqli_query($conn, $sql);
+    if ($res) {
+        header('location:return-book.php');
+    } else {
+        echo "Cannot return book.";
+    }
+}
