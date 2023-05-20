e43fbec95e7c9231dd31321703249b45ef0c113c
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-20 15:37:27 +0800

    :sparkles: feat: addded card catalog

--- client/librarian/inventory-reports.php
+++ client/librarian/inventory-reports.php
@@ -4,7 +4,7 @@ include('navigation-bar.php');
 
 <!--search bar-->
 <div class="p-4 flex justify-end">
-    <form>
+    <form autocomplete="off">
         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div class="relative w-96">
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -83,10 +83,10 @@ include('navigation-bar.php');
                 ?>
 
                     <tr class="bg-white border-b text-black font-semibold">
-                        <th scope="row" class="px-6 py-2 font-semibold text-black whitespace-nowrap text-center">
+                        <td scope="row" class="px-6 py-2 font-semibold text-black whitespace-nowrap text-center">
                             <?php echo $book['book_ID'] ?>
-                        </th>
-                        <td class="px-6 py-2">
+                        </td>
+                        <td onclick="openModal(<?php echo $ID; ?>)" class="px-6 py-2 select-none hover:bg-blue-200" data-modal-target="card-modal" data-modal-toggle="card-modal">
                             <?php echo $book['title'] ?>
                         </td>
                         <td class="px-6 py-2">
@@ -128,147 +128,72 @@ include('navigation-bar.php');
     </div>
 </div>
 
-<!--register modal-->
-<div id="register-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative w-full max-w-md max-h-full">
-        <!--register modal content-->
+<!--card modal-->
+<div id="card-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative max-h-full mx-auto flex items-center justify-center w-[55rem]">
+        <!--card modal content-->
         <div class="relative bg-gray-200 rounded-lg shadow">
-            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="register-modal">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="card-modal">
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                 </svg>
                 <span class="sr-only">Close modal</span>
             </button>
             <div class="px-6 py-6 lg:px-8">
-                <h3 class="mb-4 text-xl font-medium text-gray-900 text-center">Registration Form</h3>
+                <h3 class="mb-4 text-xl font-medium text-gray-900 text-center">Card Catalog</h3>
                 <form class="space-y-6" action="#" autocomplete="off">
-                    <div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900">ID Number</label>
-                        <input type="number" name="employeeID" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" onKeyPress="if(this.value.length==15) return false;" required>
-                    </div>
+                    <table>
+                        <tr>
+                            <td id="category" class="text-center px-8 py-1"></td>
+                            <td></td>
+                        </tr>
 
-                    <div class="grid grid-cols-3 gap-4">
-                        <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900">Last Name</label>
-                            <input type="text" name="lastName" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="20" required>
-                        </div>
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">First
-                                Name</label>
-                            <input type="text" name="firstName" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="25" required>
-                        </div>
-                        <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900">Middle
-                                Initial</label>
-                            <input type="text" name="middleInitial" class="text-center bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="1" required>
-                        </div>
-                    </div>
-                    <div class="grid grid-cols-2 gap-4">
-                        <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900">Name Extension</label>
-                            <select id="nameExtension" class="bg-gray-50 border border-gray-400 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-center">
-                                <option value="" selected>Select Extension</option>
-                                <option value="Jr">Jr</option>
-                                <option value="Sr">Sr</option>
-                                <option value="I">I</option>
-                                <option value="II">II</option>
-                                <option value="III">III</option>
-                                <option value="IV">IV</option>
-                                <option value="V">V</option>
-                                <option value="VI">VI</option>
-                                <option value="VII">VII</option>
-                                <option value="VIII">VIII</option>
-                            </select>
-                        </div>
+                        <tr>
+                            <td id="authorNumber" class="text-center px-8 py-1"></td>
+                            <td></td>
+                        </tr>
 
-                        <div class="flex">
-                            <label class="block text-sm font-medium text-gray-900">Sex</label>
-                            <div class="flex items-center mb-4 mr-4">
-                                <input id="female" type="radio" value="Female" name="female" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 mt-5 ml-[-1rem]">
-                                <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 mt-5">Female</label>
-                            </div>
-                            <div class="flex items-center mb-4">
-                                <input id="male" type="radio" value="Male" name="male" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 mt-5">
-                                <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 mt-5">Male</label>
-                            </div>
-                        </div>
-                    </div>
 
-                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register
-                        Borrower</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+                        <tr>
+                            <td></td>
+                            <td id="author" class="px-8 py-1"></td>
+                        </tr>
 
-<!--edit form modal-->
-<div id="edit-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative w-full max-w-md max-h-full">
-        <!--edit form modal content-->
-        <div class="relative bg-gray-200 rounded-lg shadow">
-            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="edit-modal">
-                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                </svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-            <div class="px-6 py-6 lg:px-8">
-                <h3 class="mb-4 text-xl font-medium text-gray-900 text-center">Edit Registration Details</h3>
-                <form class="space-y-6" action="#" autocomplete="off">
-                    <div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900">ID Number</label>
-                        <input type="number" name="employeeID" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" onKeyPress="if(this.value.length==15) return false;" onscroll="" required>
-                    </div>
+                        <tr>
+                            <td></td>
+                            <td id="title" class="pl-16 px-8 py-1"></td>
+                        </tr>
 
-                    <div class="grid grid-cols-3 gap-4">
-                        <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900">Last Name</label>
-                            <input type="text" name="lastName" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="20" required>
-                        </div>
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">First
-                                Name</label>
-                            <input type="text" name="firstName" class="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="25" required>
-                        </div>
-                        <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900">Middle
-                                Initial</label>
-                            <input type="text" name="middleInitial" class="text-center bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" maxlength="1" required>
-                        </div>
-                    </div>
-                    <div class="grid grid-cols-2 gap-4">
-                        <div>
-                            <label class="block mb-2 text-sm font-medium text-gray-900">Name Extension</label>
-                            <select id="nameExtension" class="bg-gray-50 border border-gray-400 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-center" required>
-                                <option value="" selected>Select Extension</option>
-                                <option value="Jr">Jr</option>
-                                <option value="Sr">Sr</option>
-                                <option value="I">I</option>
-                                <option value="II">II</option>
-                                <option value="III">III</option>
-                                <option value="IV">IV</option>
-                                <option value="V">V</option>
-                                <option value="VI">VI</option>
-                                <option value="VII">VII</option>
-                                <option value="VIII">VIII</option>
-                            </select>
-                        </div>
+                        <tr>
+                            <td></td>
+                            <td id="publication" class="px-8 py-1"></td>
+                        </tr>
 
-                        <div class="flex">
-                            <label class="block text-sm font-medium text-gray-900">Sex</label>
-                            <div class="flex items-center mb-4 mr-4">
-                                <input id="female" type="radio" value="Female" name="female" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 mt-5 ml-[-1rem]" required>
-                                <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 mt-5">Female</label>
-                            </div>
-                            <div class="flex items-center mb-4">
-                                <input id="male" type="radio" value="Male" name="male" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 mt-5" required>
-                                <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 mt-5">Male</label>
-                            </div>
-                        </div>
-                    </div>
-                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update
-                        Details</button>
+                        <tr>
+                            <td></td>
+                            <td id="physical" class="px-8 py-1"></td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td id="isbn" class="px-8 py-1"></td>
+                        </tr>
+
+                        <tr>
+                            <td id="accessionNumber" class="px-8 py-1"></td>
+                            <td></td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td id="subject" class="px-8 py-1"></td>
+                        </tr>
+
+                        <tr>
+                            <td></td>
+                            <td id="tracing" class="px-8 py-1"></td>
+                        </tr>
+                    </table>
                 </form>
             </div>
         </div>
@@ -319,6 +244,40 @@ include('navigation-bar.php');
                 }
             }
         }
+    }
+</script>
+<script>
+    // Function to open the modal
+    function openModal(id) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'get-data.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+
+                // Update the modal content with the retrieved data
+                document.getElementById('category').textContent = data.category;
+                document.getElementById('authorNumber').textContent = data.author_number
+                document.getElementById('author').textContent = data.author_lastname + ', ' + data.author_firstname;
+                document.getElementById('accessionNumber').textContent = data.accession_number;
+                document.getElementById('title').textContent = data.title + ' / ' + data.author_firstname + ' ' + data.author_lastname;
+                document.getElementById('publication').textContent = data.publisher + ' -- ' + data.publication_place + ', C ' + data.copyright_year;
+                document.getElementById('physical').textContent = data.physical_description;
+                document.getElementById('isbn').textContent = 'ISBN ' + data.isbn;
+                document.getElementById('subject').textContent = data.subject;
+                document.getElementById('tracing').textContent = data.tracing;
+            } else {
+                console.error('Request failed. Status: ' + xhr.status);
+            }
+        };
+
+        xhr.onerror = function() {
+            console.error('Request failed. Network error.');
+        };
+
+        xhr.send('id=' + id);
     }
 </script>
 </body>
