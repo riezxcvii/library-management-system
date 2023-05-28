decfb049231681f934a05bec247e6f3a51464b57
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-28 16:38:56 +0800

    :card_file_box: feat: logout for borrowers recorded to db

---
+++ server/borrower/logout.php
@@ -0,0 +1,6 @@
+<?php
+include "../db/conDB.php";
+$sql = "UPDATE `log_history` SET time_out = NOW() WHERE date = CURDATE() ORDER BY time_in DESC LIMIT 1 ";
+mysqli_query($conn, $sql);
+session_destroy();
+header("Location: ../../client/index2.php");
