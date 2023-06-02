d1ea93d09f11881fcd87f37b7e9d9a0c30f75b5b
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-06-02 15:13:26 +0800

    :lipstick: fix: replace buttons with icons

--- client/admin/account-registration.php
+++ client/admin/account-registration.php
@@ -91,9 +91,10 @@ include('navigation-bar.php');
                         </td>
                         <td class="px-6 py-2 justify-center flex">
                             <a href="./approve-decline.php?id=<?php echo $id ?>">
-                                <button type="button" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300">
-                                    Details
-                                </button>
+                                <svg class="w-6 text-black items-center align-middle my-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                </svg>
                             </a>
                         </td>
                     </tr>
