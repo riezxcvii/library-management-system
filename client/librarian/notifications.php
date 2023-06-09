55d66a71c7cd5da225d753b636e33b62fe51249c
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-06-09 18:03:20 +0800

    :sparkle: feat: notification page

---
+++ client/librarian/notifications.php
@@ -0,0 +1,41 @@
+<?php
+include('navigation-bar.php');
+?>
+
+<div class="mb-4 flex justify-center items-center mt-[-1rem]">
+    <div class="bg-gray-50 rounded-md m-12 my-8 w-[33rem] min-h-screen sm:min-h-[75.5vh]">
+        <div class="px-4 py-6 lg:px-8">
+            <div class="flex justify-between">
+                <h3 class="mb-4 text-xl font-medium text-gray-900">Notifications</h3>
+                <a href="" class="text-xs text-blue-600 underline mt-[0.3rem]">Mark all as read</a>
+            </div>
+            <form class="space-y-6" action="#" autocomplete="off" method="POST">
+                <ul>
+                    <li>
+                        <!--notif for book due-->
+                        <div class="bg-blue-100 p-4 text-sm rounded-md mb-1">
+                            <h2 class="font-semibold">Book Due</h2>
+                            <h3>Romeo and Juliet borrowed by Rieza Marie Banquillo is already due.</h3>
+                        </div>
+                        <!--notif for book availability-->
+                        <div class="bg-blue-100 p-4 text-sm rounded-md mb-1">
+                            <h2 class="font-semibold">Book Availability</h2>
+                            <h3>Low stock for Romeo and Juliet book.</h3>
+                        </div>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="flex justify-end pr-4 pb-2 mt-[-1.5rem]">
+    <button onclick="history.back()" type="button" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-red-700 focus:ring-2 focus:outline-none focus:ring-red-300">
+        Back
+    </button>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+</body>
+
+</html>
