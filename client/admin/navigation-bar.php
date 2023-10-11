502df9d3f76bd2aa98e23158e3d926dfbfd9d96c
rieza-ix <rizmarezlo@gmail.com>
2023-10-11 15:38:54 +0800

    :iphone::bulb: fix: mobile responsiveness for navigation bar and add comments

--- client/admin/navigation-bar.php
+++ client/admin/navigation-bar.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ANS LMS - Admin</title>
+    <title>Dashbaord - Admin</title>
     <link href="../assets/logo.png" type="image/x-icon" rel="shortcut icon">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
@@ -13,7 +13,6 @@
 
     <!-- CSS for print styles -->
     <style>
-        /* Styles for printing */
         @media print {
             body {
                 margin: 1;
@@ -61,21 +60,22 @@
 
 <body>
     <div class="h-screen flex flex-col bg-gradient-to-b from-blue-400 to-white">
-        <!--navigation bar-->
-        <nav class="z-10 sticky top-0 bg-black border-gray-200 dark:border-black">
+        <!-- Navigation Bar -->
+        <nav class="z-10 sticky top-0 bg-black border-gray-200">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
                 <a href="dashboard.php" class="flex items-center">
-                    <img src="../assets/logo.png" class="h-12 mr-3" alt="ANS Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">ANS Library
+                    <img src="../assets/logo.png" class="md:h-12 h-9 mr-3" alt="ANS Logo" />
+                    <span class="self-center font-semibold whitespace-nowrap text-white md:text-2xl text-base">ANS Library
                         Management System</span>
                 </a>
-                <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+                <!-- Humberger Menu -->
+                <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100" aria-controls="navbar-dropdown" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
                     </svg>
                 </button>
-
+                <!-- SQL Query for login validation -->
                 <?php
                 session_start();
                 include "../../server/db/conDB.php";
@@ -93,34 +93,37 @@
                 $p = mysqli_fetch_assoc($notifQuery);
                 $Count = $p['total'];
                 ?>
-
-                <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:border-gray-700">
+                <!-- Navigation Links -->
+                <div class="hidden w-full md:block md:w-auto flex justify-end" id="navbar-dropdown">
+                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-black md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:border-gray-700 md:w-auto w-fit">
                         <li>
                             <a href="dashboard.php" class="block py-2 pl-3 pr-4 bg-blue-700 rounded md:bg-transparent text-white md:p-0" aria-current="page">
-                                <svg class="w-6 h-auto" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <svg class="w-6 h-auto hidden md:block" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
                                 </svg>
+                                <span class="self-center font-semibold whitespace-nowrap text-white text-sm md:hidden block">Home</span>
                             </a>
                         </li>
-                        <!--notifications-->
+                        <!-- Notifications -->
                         <li>
                             <a href="./notifications.php" class="relative">
                                 <button id="dropdownNavbarLink1" class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-white rounded md:border-0 hover:text-blue-400 md:p-0 md:w-auto">
-                                    <svg class="w-[1.6rem] h-auto" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                    <svg class="w-[1.6rem] h-auto hidden md:block" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"></path>
                                     </svg>
-                                    <span class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-3 -right-5 dark:border-gray-900 px-3"><?php echo $Count; ?></span>
+                                    <span class="absolute invisible md:visible inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-3 -right-5 dark:border-gray-900 px-3"><?php echo $Count; ?></span>
+                                    <span class="self-center font-semibold whitespace-nowrap text-white text-sm md:hidden block">Notifications</span>
                                 </button>
                             </a>
                         </li>
+                        <!-- User name and dropdown button -->
                         <li>
-                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-white rounded md:border-0 hover:text-blue-400 md:p-0 md:w-auto"><span><?php echo $first; ?> <?php echo $last; ?></span> <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-white rounded md:border-0 hover:text-blue-400 md:p-0 md:w-auto"><span class="md:text-base text-sm"><?php echo $first; ?> <?php echo $last; ?></span> <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                                 </svg></button>
-                            <!-- Dropdown menu -->
+                            <!-- Dropdown Menu -->
                             <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:divide-gray-600">
-                                <ul class="py-2 text-sm text-black" aria-labelledby="dropdownLargeButton">
+                                <ul class="text-sm text-black" aria-labelledby="dropdownLargeButton">
                                     <div class="py-1" data-modal-target="popup-modal" data-modal-toggle="popup-modal">
                                         <a href="#" class="block px-4 py-2 text-sm text-black hover:bg-blue-100 hover:text-black">Sign
                                             out</a>
@@ -133,10 +136,11 @@
             </div>
         </nav>
 
-        <!--modal for sign out link-->
-        <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <!-- Modal for sign out link -->
+        <div id="popup-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden p-8 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
             <div class="relative w-full max-w-md max-h-full">
                 <div class="relative bg-white rounded-lg shadow">
+                    <!-- Close button (x) -->
                     <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -144,10 +148,10 @@
                         <span class="sr-only">Close modal</span>
                     </button>
                     <div class="p-6 text-center">
-                        <svg aria-hidden="true" class="mx-auto mb-4 text-gray-700 w-14 h-14" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <svg aria-hidden="true" class="mx-auto md:mb-4 mb-1 text-gray-700 md:w-14 w-11 h-14" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                         </svg>
-                        <h3 class="mb-5 text-lg font-normal text-gray-900">Are you sure you want to
+                        <h3 class="mb-5 md:text-lg text-base font-normal text-gray-900">Are you sure you want to
                             sign out?</h3>
                         <a href="../../server/admin/logout.php?id=<?php echo $admin_ID; ?>">
                             <button type="button" class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
