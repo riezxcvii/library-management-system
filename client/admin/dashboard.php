b648389aa62fb1b36e9936ca1cc696c8a57b94bd
rieza-ix <rizmarezlo@gmail.com>
2023-10-11 18:36:20 +0800

    :iphone: fix: mobile responsiveness for admin dashboard

--- client/admin/dashboard.php
+++ client/admin/dashboard.php
@@ -2,46 +2,46 @@
 include('navigation-bar.php');
 ?>
 
-<!--cards-->
-<div class="flex justify-center items-center flex-1">
+<!-- Cards -->
+<div class="flex justify-center items-center flex-1 px-9 md:pb-0 pb-6 md:pt-0 pt-9">
     <div class="inline-flex flex-wrap gap-4 sm:gap-12">
-        <!--log history card-->
-        <div class="w-full sm:w-72 p-4 bg-black border border-gray-200 rounded-lg shadow sm:p-8 dark:border-gray-700">
-            <h5 class="mb-4 text-xl text-center font-semibold text-white">Log History</h5>
-            <div class="mb-8 w-full flex items-baseline justify-center text-gray-900 dark:text-white text-center">
-                <span class="text-base text-gray-400 font-semiabold">View users log in details.</span>
+        <!-- Log history card -->
+        <div class="w-full sm:w-72 p-4 bg-black border border-gray-200 rounded-lg shadow sm:p-8 dark:border-gray-700 mb-4 md:mb-0">
+            <h5 class="mb-4 md:text-xl text-base text-center font-semibold text-white">Log History</h5>
+            <div class="md:mb-8 mb-6 w-full flex items-baseline justify-center text-gray-900 dark:text-white text-center">
+                <span class="md:text-base text-sm text-gray-400 font-semiabold">View users log in details.</span>
             </div>
             <a href="log-history.php">
-                <button type="button" class="text-white bg-blue-500 hover:bg-blue-500 focus:ring-1 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full sm:text-center">View</button>
+                <button type="button" class="text-white bg-blue-500 hover:bg-blue-500 focus:ring-1 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 md:py-2.5 py-2 inline-flex justify-center w-full sm:text-center mb-1 md:mb-0">View</button>
             </a>
         </div>
-        <!--account management card-->
-        <div class="w-full sm:w-72 p-4 bg-black border border-gray-200 rounded-lg shadow sm:p-8 dark:border-gray-700">
-            <h5 class="mb-4 text-xl text-center font-semibold text-white">Account Management</h5>
-            <div class="mb-8 w-full flex items-baseline justify-center text-gray-900 dark:text-white text-center">
-                <span class="text-base text-gray-400 font-semiabold">Manage registrations.</span>
+        <!-- Account management card -->
+        <div class="w-full sm:w-72 p-4 bg-black border border-gray-200 rounded-lg shadow sm:p-8 dark:border-gray-700 mb-4 md:mb-0">
+            <h5 class="mb-4 md:text-xl text-base text-center font-semibold text-white">Account Management</h5>
+            <div class="md:mb-8 mb-6 w-full flex items-baseline justify-center text-gray-900 dark:text-white text-center">
+                <span class="md:text-base text-sm text-gray-400 font-semiabold">Manage registrations.</span>
             </div>
-            <div class="flex gap-4 justify-center">
-                <a href="librarian-registration.php">
-                    <button type="button" class="text-white bg-blue-500 hover:bg-blue-500 focus:ring-1 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full sm:text-center">Librarian</button>
+            <div class="flex gap-4 justify-center mb-1 md:mb-0">
+                <a href="librarian-registration.php" class="w-full">
+                    <button type="button" class="text-white bg-blue-500 hover:bg-blue-500 focus:ring-1 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 md:py-2.5 py-2 inline-flex justify-center w-full sm:text-center">Librarian</button>
                 </a>
-                <a href="account-registration.php">
-                    <button type="button" class="text-white bg-blue-500 hover:bg-blue-500 focus:ring-1 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full sm:text-center">Borrower</button>
+                <a href="account-registration.php" class="w-full">
+                    <button type="button" class="text-white bg-blue-500 hover:bg-blue-500 focus:ring-1 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 md:py-2.5 py-2 inline-flex justify-center w-full sm:text-center">Borrower</button>
                 </a>
             </div>
         </div>
-        <!--account reports card-->
-        <div class="w-full sm:w-72 p-4 bg-black border border-gray-200 rounded-lg shadow sm:p-8 dark:border-gray-700">
-            <h5 class="mb-4 text-xl text-center font-semibold text-white">Account Reports</h5>
-            <div class="mb-8 w-full flex items-baseline justify-center text-gray-900 dark:text-white text-center">
-                <span class="text-base text-gray-400 font-semiabold">Manage registered users.</span>
+        <!-- Account reports card -->
+        <div class="w-full sm:w-72 p-4 bg-black border border-gray-200 rounded-lg shadow sm:p-8 dark:border-gray-700 mb-4 md:mb-0">
+            <h5 class="mb-4 md:text-xl text-base text-center font-semibold text-white">Account Reports</h5>
+            <div class="md:mb-8 mb-6 w-full flex items-baseline justify-center text-gray-900 dark:text-white text-center">
+                <span class="md:text-base text-sm text-gray-400 font-semiabold">Manage registered users.</span>
             </div>
-            <div class="flex gap-4 justify-center">
-                <a href="librarian-accounts.php">
-                    <button type="button" class="text-white bg-blue-500 hover:bg-blue-500 focus:ring-1 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full sm:text-center">Librarian</button>
+            <div class="flex gap-4 justify-center mb-1 md:mb-0">
+                <a href="librarian-accounts.php" class="w-full">
+                    <button type="button" class="text-white bg-blue-500 hover:bg-blue-500 focus:ring-1 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 md:py-2.5 py-2 inline-flex justify-center w-full sm:text-center">Librarian</button>
                 </a>
-                <a href="account-reports.php">
-                    <button type="button" class="text-white bg-blue-500 hover:bg-blue-500 focus:ring-1 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full sm:text-center">Borrower</button>
+                <a href="account-reports.php" class="w-full">
+                    <button type="button" class="text-white bg-blue-500 hover:bg-blue-500 focus:ring-1 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 md:py-2.5 py-2 inline-flex justify-center w-full sm:text-center">Borrower</button>
                 </a>
             </div>
         </div>
