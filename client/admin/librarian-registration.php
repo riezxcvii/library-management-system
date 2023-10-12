732aeab2b91be72c8d188625d66bdcd43bb812c5
rieza-ix <rizmarezlo@gmail.com>
2023-10-12 11:21:34 +0800

    :fire: fix: remove unused data-toggle

--- client/admin/librarian-registration.php
+++ client/admin/librarian-registration.php
@@ -6,7 +6,7 @@ include('navigation-bar.php');
 <div class="p-4 pt-6 flex justify-end">
     <!-- Add button -->
     <a href="./add-librarian.php">
-        <button type="button" class="h-[2.55rem] md:mr-8b mr-7 inline-flex items-center px-5 py-2.5 md:text-base text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300" data-modal-target="register-modal" data-modal-toggle="register-modal">
+        <button type="button" class="h-[2.55rem] md:mr-8b mr-7 inline-flex items-center px-5 py-2.5 md:text-base text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300">
             Add Account
         </button>
     </a>
