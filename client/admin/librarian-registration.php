9be2b44262e5733704f9c442e5169dde73f85f52
rieza-ix <rizmarezlo@gmail.com>
2023-10-12 13:34:19 +0800

    :truck: fix: rename update-librarian to update-librarian-account

--- client/admin/librarian-registration.php
+++ client/admin/librarian-registration.php
@@ -84,7 +84,7 @@ include('navigation-bar.php');
                     <?php echo $name['role'] ?>
                 </td>
                 <td class="px-6 py-2 justify-center flex">
-                    <a href="./update-librarian.php?id=<?php echo $id ?>">
+                    <a href="./update-librarian-account.php?id=<?php echo $id ?>">
                         <svg class="w-6 text-black items-center align-middle my-2 mr-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
                         </svg>
