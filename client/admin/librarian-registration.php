edc41885e63445f214c2a3a106e64a7c237b2a7c
rieza-ix <rizmarezlo@gmail.com>
2023-10-12 14:35:41 +0800

    :iphone: fix: add and back button

--- client/admin/librarian-registration.php
+++ client/admin/librarian-registration.php
@@ -6,7 +6,7 @@ include('navigation-bar.php');
 <div class="p-4 pt-6 flex justify-end">
     <!-- Add button -->
     <a href="./add-librarian-account.php">
-        <button type="button" class="h-[2.55rem] md:mr-8b mr-7 inline-flex items-center px-5 py-2.5 md:text-base text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300">
+        <button type="button" class="h-[2.55rem] md:mr-8b mr-7 inline-flex items-center md:px-5 px-3 md:py-2.5 py-1 md:text-base text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-2 focus:outline-none focus:ring-blue-300">
             Add Account
         </button>
     </a>
@@ -23,9 +23,8 @@ include('navigation-bar.php');
         </div>
     </form>
 </div>
-
 <!-- Table -->
-<div class="px-4 pt-2 pb-6 h-screen overflow-y-auto ">
+<div class="px-4 pt-2 pb-20 h-screen overflow-y-auto ">
     <div class="relative overflow-y-auto h-full bg-white shadow-md rounded-lg">
         <table id="table" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="sticky top-0 text-xs text-white uppercase bg-black">
@@ -62,52 +61,43 @@ include('navigation-bar.php');
                     </th>
             </thead>
             <tbody>
-            <?php
-            $borrower = $conn->query("SELECT * FROM `library_admin` WHERE deactivate=0 ORDER BY last_name ASC") or die(mysqli_error($conn));
-            while ($name = $borrower->fetch_array()) {
-                $id = $name['admin_ID'];
-                $last = $name['last_name'];
-                $first = $name['first_name'];
-                $middle = $name['middle_initial'];
-                $extension = $name['name_extension'];
-                $sex = $name['sex'];
-                $role = $name['role'];
-            ?>
-            <tr class="bg-white border-b text-black font-semibold">
-                <td scope="row" class="px-6 py-2 font-semibold text-black whitespace-nowrap">
-                    <?php echo $name['last_name'] ?>
-                </td>
-                <td class="px-6 py-2">
-                    <?php echo $name['first_name'] ?>
-                </td>
-                <td class="px-6 py-2">
-                    <?php echo $name['role'] ?>
-                </td>
-                <td class="px-6 py-2 justify-center flex">
-                    <a href="./update-librarian-account.php?id=<?php echo $id ?>">
-                        <svg class="w-6 text-black items-center align-middle my-2 mr-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
-                        </svg>
-                    </a>
-                    <a href="./view-librarian-account.php?id=<?php echo $id ?>">
-                        <svg class="w-6 text-black items-center align-middle my-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                        </svg>
-                    </a>
-                </td>
-            </tr>
+                <!-- SQL Query for dispalying data in the table -->
+                <?php
+                $borrower = $conn->query("SELECT * FROM `library_admin` WHERE deactivate=0 ORDER BY last_name ASC") or die(mysqli_error($conn));
+                while ($name = $borrower->fetch_array()) {
+                    $id = $name['admin_ID'];
+                    $last = $name['last_name'];
+                    $first = $name['first_name'];
+                    $middle = $name['middle_initial'];
+                    $extension = $name['name_extension'];
+                    $sex = $name['sex'];
+                    $role = $name['role'];
+                ?>
+                <tr class="bg-white border-b text-black font-semibold">
+                    <td scope="row" class="px-6 py-2 font-semibold text-black whitespace-nowrap">
+                        <?php echo $name['last_name'] ?>
+                    </td>
+                    <td class="px-6 py-2">
+                        <?php echo $name['first_name'] ?>
+                    </td>
+                    <td class="px-6 py-2">
+                        <?php echo $name['role'] ?>
+                    </td>
+                    <td class="px-6 py-2 justify-center flex">
+                        <a href="./update-librarian-account.php?id=<?php echo $id ?>">
+                            <svg class="w-6 text-black items-center align-middle my-2 mr-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>
+                            </svg>
+                        </a>
+                        <a href="./view-librarian-account.php?id=<?php echo $id ?>">
+                            <svg class="w-6 text-black items-center align-middle my-2" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path>
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                            </svg>
+                        </a>
+                    </td>
+                </tr>
             </tbody>
-            <?php
-                if (isset($_POST['decline'])) {
-                    $sql = "UPDATE `borrowers` SET status=2 WHERE borrower_ID = '$id'";
-                    $res = mysqli_query($conn, $sql);
-                }
-                if (isset($_POST['approve'])) {
-                    $sql = "UPDATE `borrowers` SET status=1 WHERE borrower_ID = $id";
-                    $res = mysqli_query($conn, $sql);
-                }
-            ?>
         <?php
             }
         ?>
@@ -115,7 +105,7 @@ include('navigation-bar.php');
     </div>
 </div>
 
-<div class="flex justify-end pr-4 pb-4">
+<div class="flex justify-end pr-4 pb-4 fixed bottom-0 right-0">
     <a href="dashboard.php">
         <button type="button" class="inline-flex items-center px-5 py-2 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-red-700 focus:ring-2 focus:outline-none focus:ring-red-300">
             Back
