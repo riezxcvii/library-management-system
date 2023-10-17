aa2f8d9e0f90184362ce1c6e5a36a98da352e5d4
rieza-ix <rizmarezlo@gmail.com>
2023-10-17 18:20:02 +0800

    :bug: fix: username input type change to text instead of username

--- client/index.php
+++ client/index.php
@@ -29,7 +29,7 @@ session_start();
                 <div class="mb-6">
                     <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Enter
                         username</label>
-                    <input type="username" id="username" name="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mt-2" minlength="5" maxlength="30" required>
+                    <input type="text" id="username" name="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mt-2" minlength="5" maxlength="30" required>
                 </div>
                 <div class="mb-6">
                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Enter
